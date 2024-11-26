039e55ca695f4bbd5469e10b7a66a6eb454ad212
thomaskavi <thomaskavi2000@gmail.com>
2024-11-26 13:19:49 -0300

    First commit

---
+++ src/main/java/com/thomaskavi/api_restful_crud_clientes/ApiRestfulCrudClientesApplication.java
@@ -0,0 +1,13 @@
+package com.thomaskavi.api_restful_crud_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulCrudClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulCrudClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomaskavi/api_restful_crud_clientes/ApiRestfulCrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomaskavi.api_restful_crud_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulCrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
