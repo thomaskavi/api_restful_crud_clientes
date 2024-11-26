51a072159a84406b885acbeaec1a77f47942d769
thomaskavi <thomaskavi2000@gmail.com>
2024-11-26 18:15:58 -0300

    Database seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Thomas Kavi', '32165498700', 3000.0, '2000-12-20', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana Souza', '12345678901', 4500.0, '1995-03-15', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Silva', '98765432100', 5500.0, '1988-07-22', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Mariana Lima', '45678912345', 6000.0, '1990-11-10', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João Pedro', '32198765400', 3200.0, '1985-05-05', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernanda Costa', '78912345678', 4200.0, '1998-01-20', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Roberto Santos', '15975348620', 7200.0, '1983-09-18', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Paula Almeida', '85274196300', 5100.0, '1992-06-25', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas Oliveira', '96325874102', 9500.0, '1980-02-14', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Sofia Martins', '74185296347', 5800.0, '1997-10-03', 1);
