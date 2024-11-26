068feabacf9fcef661d782b88e7e14f125abf5d9
thomaskavi <thomaskavi2000@gmail.com>
2024-11-26 13:26:08 -0300

    Entity Client

---
+++ src/main/java/com/thomaskavi/api_restful_crud_clientes/entities/Client.java
@@ -0,0 +1,91 @@
+package com.thomaskavi.api_restful_crud_clientes.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  @Column(unique = true)
+  private String cpf;
+
+  private Double income;
+
+  private LocalDate birhDate;
+
+  private Integer children;
+
+  public Client() {
+  }
+
+  public Client(Long id, String name, String cpf, Double income, LocalDate birhDate, Integer children) {
+    this.id = id;
+    this.name = name;
+    this.cpf = cpf;
+    this.income = income;
+    this.birhDate = birhDate;
+    this.children = children;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public Double getIncome() {
+    return income;
+  }
+
+  public void setIncome(Double income) {
+    this.income = income;
+  }
+
+  public LocalDate getBirhDate() {
+    return birhDate;
+  }
+
+  public void setBirhDate(LocalDate birhDate) {
+    this.birhDate = birhDate;
+  }
+
+  public Integer getChildren() {
+    return children;
+  }
+
+  public void setChildren(Integer children) {
+    this.children = children;
+  }
+
+}
